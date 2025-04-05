fd62ca271c67e5500047a87c133e6b98b4c7c40f
[4기-1반] 양권민 <didrnjsals@gmail.com>
2025-04-05 22:47:44 +0900

    fix: add setScrollIndex(0) when Topic page mounted

--- src/pages/Quiz/Topic.tsx
+++ src/pages/Quiz/Topic.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setScrollIndex } from "../../store/quizSlice";
 const QUIZ_TOPICS = [
   { id: "AWS_DVA", name: "AWS Certified Developer - Associate(DVA-C02)" },
   {
@@ -9,6 +11,11 @@ const QUIZ_TOPICS = [
 ];
 
 export default function QuizListPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setScrollIndex(0));
+  }, [dispatch]);
+
   return (
     <main className="flex flex-col items-center justify-center p-4">
       <h1 className="text-xl font-bold mb-4 text-center">문제 유형 선택</h1>
